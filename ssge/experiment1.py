a48781dd291f4f012caf9c57a1e0abcf57ded0fc
Michael Downs <michaeldowns@Michaels-MacBook-Pro.local>
2019-12-10 20:04:36 -0500

    added gmm module

--- ssge/experiment1.py
+++ ssge/experiment1.py
@@ -1,43 +1,13 @@
 """
-1D multimodal distribution
+In this experiment, we assess how well the
+SSGE performs for a 1D Gaussian Mixture as we vary different
+Gaussian Mixture parameters
 """
 
 from ssge import SSGE
 
-import jax.numpy as jnp
-from jax.scipy import stats as jsps
-from jax import grad, vmap
-
 import matplotlib.pyplot as plt
-import numpy as np
 
-
-
-def gmm_pdf(x, weights, mus, sigmas):
-
-    pdfval = 0
-
-    for weight, mu, sigma in zip(weights, mus, sigmas):
-        pdfval = pdfval + weight * jsps.norm.pdf(x, mu, sigma)
-
-    return pdfval
-
-
-def log_gmm_pdf(x, weights, mus, sigmas):
-    return jnp.log(gmm_pdf(x, weights, mus, sigmas))
-
-def sample_gmm(num_samples, weights, mus, sigmas):
-    num_pdfs = len(weights)
-
-    mask = np.random.multinomial(1, weights, size=num_samples)
-
-    all_samples = np.random.normal(loc=mus,
-                                   scale=sigmas,
-                                   size=(num_samples, num_pdfs))
-
-    masked_samples = mask * all_samples
-
-    return masked_samples.sum(axis=1).flatten()
 
 
 def main():
@@ -62,8 +32,9 @@ def main():
     ax.plot(x, gmm_pdf(x, weights, mus, sigmas),
             label='Analytic PDF')
 
+    print("jax")
     deriv_func = lambda x: deriv_grad_log_density(x, weights, mus, sigmas)
-    ax.plot(x, list(map(deriv_func, x)), label='Analytic Density')
+    ax.plot(x, list(map(deriv_func, x)), label='Analytic GLD')
 
     ax.scatter(samples, np.zeros(num_samples),
                color='red',
